78ceccab270489f104e397d1287d45bdc9e02fb0
jaldamaj <jaldamaj@gmail.com>
2015-01-13 22:38:53 +0100

    Evitar nicknames duplicados

--- io.js
+++ io.js
@@ -5,13 +5,31 @@ module.exports = function(io) {
 		socket.on("set_nickname", function(nickname, callback) {
 			console.log("Trying to set nickname " + nickname);
 
-			socket.nickname = nickname;
+			var isAvailable = isNicknameAvailable(nickname);
 
-			callback(true);
+			if (isAvailable)
+				socket.nickname = nickname;
+
+			callback(isAvailable);
 		});
 
 		socket.on("message", function(message) {
 			io.sockets.emit("message", socket.nickname, message);
 		});
+
+		var isNicknameAvailable = function(nickname) {
+			var clients = io.sockets.sockets;
+
+			for (var client in clients) {
+				if (clients.hasOwnProperty(client)) {
+					client = clients[client];
+
+					if (client.nickname == nickname)
+						return false;
+				}
+			}
+
+			return true;
+		}
 	});
 }
